f547a182d5cc5cb823d1f54742abd5b452a4a936
osataken <osataken@gmail.com>
2023-08-28 13:11:28 +0700

    update gitignore, and package.json

--- lib/cdk-s3-lambda-dynamodb-stack.ts
+++ lib/cdk-s3-lambda-dynamodb-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class CdkS3LambdaDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const incomingBucket = new s3.Bucket(this, 'IncomingBucket', {
+        bucketName: "fwd-excel-file-upload-08222023",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3LambdaDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fileProcessingLambda = new lambda.Function(this, 'FileProcessingLambda', {
+        functionName:  'FileProcessingLambda',
+        runtime: lambda.Runtime.NODEJS_16_X,
+        code: lambda.Code.fromAsset('lambda'),
+        timeout:  cdk.Duration.seconds(300),
+        handler: 'file_processing.handler',
+    });
+
+    const transactionTable = new dynamodb.Table(this, 'TransactionTable', {
+        tableName: 'BookCatalogue',
+        partitionKey: {
+            name: 'book_id',
+            type: dynamodb.AttributeType.STRING,
+        }
+    });
+
+    incomingBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.LambdaDestination(fileProcessingLambda));
+    incomingBucket.grantRead(fileProcessingLambda);
+
+    transactionTable.grantReadWriteData(fileProcessingLambda);
   }
 }
